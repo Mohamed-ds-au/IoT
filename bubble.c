b2ce6b3febff2896486e9f31ad8610d6d45e16ee
Mohamed-ds-au <111690029+Mohamed-ds-au@users.noreply.github.com>
2024-08-23 12:42:02 +0300

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -1,21 +1,59 @@
 #include <stdio.h>
 void sort(int numbers[]);
-void bubbleSort(int numbers[]);
+void bubbleSort(int numbers[], int size);
 int main()
 {
-    int numbers[] = {5, 9, 6, 4, 8};
-    //sort(numbers);
+    // Take the size of the array from user
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
 
-    bubbleSort(numbers);
+    int numbers[size];
 
-    //test sorting
-    for(int i = 0; i < 5; i++)
+    // Take the numbers of the array from user
+    printf("Enter the array numbers: ");
+
+    for (int i = 0; i < size; i++)
     {
-        printf("%d", numbers[i]);
+        scanf("%d", &numbers[i]);
     }
+
+    //calling the bubble sort function
+    bubbleSort(numbers, size);
+    
+    //test sorting
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d ", numbers[i]);
+    }
+
 }
 
+// bubble sort function implementaion 
+void bubbleSort(int numbers[], int size)
+{
+    int swap;
+    for (int i = 0; i < size; i++)
+    {
+        swap = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if(numbers[j] > numbers[j+1] && j+1 != size)
+            {
+                int temp = numbers[j];
+                numbers[j] = numbers[j+1];
+                numbers[j+1] = temp; 
+                swap = 1;
+            }
+        }
 
+        if (swap == 0)
+        {
+            break;
+        }
+    }
+    
+}
 
 void sort(int numbers[])
 {
@@ -32,32 +70,4 @@ void sort(int numbers[])
             }
         }
     }
-}
-
-void bubbleSort(int numbers[])
-{
-    int swap = 1;
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if(numbers[j] < numbers[j+1] && j+1 != 5)
-            {
-                int temp = numbers[j];
-                numbers[j] = numbers[j+1];
-                numbers[j+1] = temp; 
-                swap = 1;
-            }
-            else
-            {
-                swap = 0;
-            }
-        }
-
-        if (swap = 0)
-        {
-            break;
-        }
-    }
-    
 }
